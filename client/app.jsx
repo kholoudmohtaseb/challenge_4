9cebe589d67480640262d39e2f82552f963bc1aa
kholoudmohtaseb <kholoud.almohtseb@gmail.com>
2020-11-05 15:26:01 +0200

    WTH is this

--- client/app.jsx
+++ client/app.jsx
@@ -1,2 +1,6 @@
-
+var row = document.getElementsByTagName('tr');
+var col = document.getElementsByTagName('td');
+var slots = document.getElementsByClassName('slot');
+var playerTurn = document.getElementsByClassName('player-turn');
+var reset = document.getElementsByClassName('reset');
 
